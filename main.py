f14b950fd7036501875b2d2f090da819c36421f5
klld <kll.d.repl.co@hotmail.com>
2022-12-30 20:28:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import asyncio
+import os
+import sys
 
+env_vars = {}
+if os.path.isfile('.device'):
+    with open('.device') as f:
+        for line in f:
+            if line.startswith('#') or not line.strip():
+                continue
+            key, value = line.strip().split('=', 1)
+            env_vars[key] = value.replace("\"", "")
+else:
+  print("error device in the file")
+  sys.exit()
+
+os.system('pip install klldv5')
+os.system('clear')
+
+import klldv5
+
+client = klldv5.klldv5(
+  device_id=env_vars['DEVICE_ID'],
+  account_id=env_vars['ACCOUNT_ID'],
+  secret=env_vars['SECRET']
+)
